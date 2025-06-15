26487d313e021d5273ceaf5bc2a5e4408ac3319e
balaraju <balarajugandham1@gmail.com>
2025-06-15 14:13:36 +0530

    changes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const sidebar=document.getElementById("sidebar");
-const closeBtn=document.getElementById("closeBtn");
-const closeDiv =document.getElementById("close");
-const barsBtn=document.getElementById("bars");
+const sidebar = document.getElementById("sidebar");
+const closeBtn = document.getElementById("closeBtn");
+const closeDiv = document.getElementById("close");
+const barsBtn = document.getElementById("bars");
 
-closeBtn.addEventListener("click",function(){
-    closeDiv.style.display=`none`
-    sidebar.style.width=`0px`;
+closeBtn.addEventListener("click", function () {
+ closeDiv.style.display = `none`
+ sidebar.style.width = `0px`;
 })
 
-barsBtn.addEventListener("click",function(){
-    closeDiv.style.display=`flex`
-    sidebar.style.width=`70%`
+barsBtn.addEventListener("click", function () {
+ closeDiv.style.display = `flex`
+ sidebar.style.width = `70%`
 })
